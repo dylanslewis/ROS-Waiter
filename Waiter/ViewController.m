7c527a1141c5a8c58aa7122ad48d97d010742610
Your Name Comes Here <dylanslewis@me.com>
2014-09-10 10:36:28 +0100

    Initial Commit

---
+++ Waiter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Waiter
+//
+//  Created by Dylan Lewis on 10/09/2014.
+//  Copyright (c) 2014 Dylan Lewis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
